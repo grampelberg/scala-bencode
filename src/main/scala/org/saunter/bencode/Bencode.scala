07eb24e00a8db930c6203c792841a099d6a077c3
Thomas Rampelberg <trampelb@grue.dark>
2009-08-21 15:38:00 -0700

    Changing the copyright email address and making the readme all pretty formatted.

--- src/main/scala/org/saunter/bencode/Bencode.scala
+++ src/main/scala/org/saunter/bencode/Bencode.scala
@@ -1,4 +1,4 @@
-/* Copyright (C) 2009 Thomas Rampelberg <pyronicide@gmail.com>
+/* Copyright (C) 2009 Thomas Rampelberg <pyronicide@saunter.org>
 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
